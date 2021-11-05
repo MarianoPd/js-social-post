9d83f39be94051da79e5caef9fea94dedb42feb0
MarianoPd <marianopidone.com>
2021-11-05 17:32:31 +0100

    Aggiunta immagine di default

--- main.js
+++ main.js
@@ -59,6 +59,7 @@ const posts = [
 const container = document.getElementById('container');
 run(posts, container);
 
+//avvia l'esecuzione partendo dall'array di elementi e dal container dove generare i post
 function run(posts, container){
     let postArray = [];
     container.innerHTML = '';
@@ -68,11 +69,70 @@ function run(posts, container){
 
 }
 
+//prende un elemento dell'array ne genera un post e lo inserisce nel container
+function generatePost(post, container){
+    
+    let info = returnPostInfo(post);
+    info.date = dateItalian(info.date);
+    info.image = checkImage(info);
+    const newPost = document.createElement('div');
+    newPost.className = 'post';
+    container.appendChild(newPost);
+    let insideContent = `
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    ${info.image}                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${info.name}</div>
+                    <div class="post-meta__time">${info.date}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${info.content}</div>
+        <div class="post__image">
+            <img src=${info.media} alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid=${info.id}>
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-${info.id}" class="js-likes-counter">${info.likes}</b> persone
+                </div>
+            </div> 
+        </div>
+    `;
+    newPost.innerHTML += insideContent;
+    setLikeClick(newPost, info);
 
 
+    return newPost;
+} 
+
+//setta il meccanismo di 'like' sul post che gli viene passato usando i dati dell'array usati per creare il post
+function setLikeClick(postHTML, info){
+    const likeBtn = postHTML.querySelector('.like-button');
+    const likeCounter = document.getElementById(`like-counter-${info.id}`); 
+    let likeCountValue = parseInt(likeCounter.innerHTML);
+    
+    likeBtn.addEventListener('click', addLike);
+
+    function addLike(event){
+        likeBtn.classList.add('like-button--liked');
+        likeCounter.innerHTML = ++likeCountValue;
+        this.removeEventListener('click', addLike);
+    }
+}
+
+//prende un elemento dell'array e restituisce un altro arraycon i dati (non essenziale ma lo preferisco)
 function returnPostInfo(post){
     let author= post["author"];
-
     let info = {
         id: post["id"],
         content: post["content"],
@@ -87,6 +147,7 @@ function returnPostInfo(post){
     return info;
 }
 
+//prende la data in formato USA e lo restituisce in formato IT
 function dateItalian(date){
     let newDate = date.substr(8,2);
     newDate += "-" + date.substr(5,2);
@@ -94,61 +155,27 @@ function dateItalian(date){
     return newDate;
 }
 
-function generatePost(post, container){
-    
-    let info = returnPostInfo(post);
-    info.date = dateItalian(info.date);
-    
-    const newPost = document.createElement('div');
-    newPost.className = 'post';
-    container.appendChild(newPost);
-    let insideContent = `
-    <div class="post__header">
-        <div class="post-meta">                    
-            <div class="post-meta__icon">
-                <img class="profile-pic" src=${info.image} alt=${info.name}>                    
-            </div>
-            <div class="post-meta__data">
-                <div class="post-meta__author">${info.name}</div>
-                <div class="post-meta__time">${info.date}</div>
-            </div>                    
-        </div>
-    </div>
-    <div class="post__text">${info.content}</div>
-    <div class="post__image">
-        <img src=${info.media} alt="">
-    </div>
-    <div class="post__footer">
-        <div class="likes js-likes">
-            <div class="likes__cta">
-                <a class="like-button  js-like-button" href="#" data-postid=${info.id}>
-                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                    <span class="like-button__label">Mi Piace</span>
-                </a>
-            </div>
-            <div class="likes__counter">
-                Piace a <b id="like-counter-${info.id}" class="js-likes-counter">${info.likes}</b> persone
-            </div>
-        </div> 
-    </div>
-    `;
-    newPost.innerHTML += insideContent;
-    setLikeClick(newPost, info);
-
-
-    return newPost;
-} 
-
-function setLikeClick(postHTML, info){
-    const likeBtn = postHTML.querySelector('.like-button');
-    const likeCounter = document.getElementById(`like-counter-${info.id}`); 
-    let likeCountValue = parseInt(likeCounter.innerHTML);
-    
-    likeBtn.addEventListener('click', addLike);
-
-    function addLike(event){
-        likeBtn.classList.add('like-button--liked');
-        likeCounter.innerHTML = ++likeCountValue;
-        this.removeEventListener('click', addLike);
+//controlla che l'immagine non si null in tal caso la modifica e restituisce l'immagine
+function checkImage(info){
+    let newImage = "";
+    if(info.image === null){
+        const initials = findInitials(info.name);
+        newImage = '<div class="profile-pic-default" style="font-size:30px; font-weight:600; color:white;">' + initials +"</div>";
+        
+    }else{
+        newImage = '<img class="profile-pic" src=' + info.image + " alt=" + info.image +">";
     }
+    console.log(newImage);
+    return newImage;
+}
+
+
+//estrae le lettere in uppercase dalla stringa name
+function findInitials(name){
+    let initials = '';
+    for(let i in name){
+        let char = name.substr(i,1);
+        if((char >= 'A') && (char <= 'Z')) initials += char;
+    }
+    return initials;
 }
