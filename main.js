33c824322924bf6ac3c9f3200769ebcef677bfde
MarianoPd <marianopidone.com>
2021-11-05 15:33:51 +0100

    base funzionate

--- main.js
+++ main.js
@@ -57,11 +57,70 @@ const posts = [
 ];
 
 const container = document.getElementById('container');
-generatePost(posts[0], container);
+run(posts, container);
+
+function run(posts, container){
+    container.innerHTML = '';
+    for(let index in posts){
+        generatePost(posts[index], container);
+    }
+}
+
+function returnPostInfo(post){
+    let author= post["author"];
+
+    let info = {
+        id: post["id"],
+        content: post["content"],
+        media: post["media"],
+        
+        name: author["name"],
+        image: author["image"],
+        likes: post["likes"],
+        created: post["created"],
+    }
+
+    return info;
+}
 
 function generatePost(post, container){
+    
+    let info = returnPostInfo(post);
+    
     const newPost = document.createElement('div');
     newPost.className = 'post';
-
     container.appendChild(newPost);
+    let insideContent = `
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src=${info.image} alt=${info.name}>                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${info.name}</div>
+                <div class="post-meta__time">${info.created}</div>
+            </div>                    
+        </div>
+    </div>
+    <div class="post__text">${info.content}</div>
+    <div class="post__image">
+        <img src=${info.media} alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid=${info.id}>
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${info.likes}</b> persone
+            </div>
+        </div> 
+    </div>
+    `;
+    newPost.innerHTML += insideContent;
+    
 } 
+
